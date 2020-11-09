58c7bbc0b0a4642eec3cda6fa6e3737a171d668e
Andrei Horbach <a_horbach@gcore.lu>
2020-11-09 22:02:17 +0300

    Strange bot win mechanic

---
+++ black_jack/Game.py
@@ -0,0 +1,159 @@
+import Player
+from Deck import Deck
+import random
+
+from const import MESSAGES
+
+
+class Game:
+    max_pl_count = 4
+
+    def __init__(self):
+        self.players = []
+        self.player = None
+        self.dealer = Player.Dealer()
+        self.player_pos = None
+        self.all_players_count = 1
+        self.deck = Deck()
+        self.max_bet, self.min_bet = 20, 0
+
+    @staticmethod
+    def _ask_starting(message):
+        while True:
+            choice = input(message)
+            if choice == 'n':
+                return False
+            elif choice == 'y':
+                return True
+
+    def _launching(self):
+        while True:
+            bots_count = int(input('Hello, write bots count '))
+            if bots_count <= self.max_pl_count - 1:  # self # Game #__name__
+                break
+
+        for _ in range(bots_count):
+            b = Player.Bot()
+            self.players.append(b)
+
+            print(b, 'is created')
+
+        self.player = Player.Player()
+        self.player_pos = random.randint(0, len(self.players))
+        print('Your position is: ', self.player_pos)  # it needs to be deleted
+        self.players.insert(self.player_pos, self.player)
+
+    def ask_bet(self):
+        for player in self.players:
+            player.change_bet(self.max_bet, self.min_bet)
+
+    def first_desc(self):
+        for player in self.players:
+            for _ in range(2):
+                card = self.deck.get_card()
+                player.take_card(card)
+
+        card = self.deck.get_card()
+        self.dealer.take_card(card)
+        self.dealer.print_cards()
+
+    def check_stop(self, player):
+        if player.full_points >= 21:
+            return True
+        else:
+            return False
+
+    def remove_player(self, player):
+        player.print_cards()
+        if isinstance(player, Player.Player):
+            print('You are fall!')
+        elif isinstance(player, Player.Bot):
+            print(player, 'are fall')
+        self.players.remove(player)
+
+
+    def ask_card(self):
+        for player in self.players:
+            while player.ask_card():
+                card = self.deck.get_card()
+                player.take_card(card)
+
+                is_stop = self.check_stop(player)
+
+                if is_stop:
+                    if player.full_points > 21 \
+                            or isinstance(player, Player.Player):
+
+                        self.remove_player(player)
+                    break
+
+                if isinstance(player, Player.Player):
+                    player.print_cards()
+
+    def check_winner(self):
+        if self.dealer.full_points > 21:
+            print(MESSAGES.get('dealer_fall'))
+            for winner in self.players:
+                winner.money += winner.bet * 2
+            # all win
+        else:
+            for player in self.players:
+                if player.full_points == self.dealer.full_points:
+                    player.money += player.bet
+                    print(MESSAGES.get('equal').format(player=player,
+                                                       points=player.full_points))
+                elif player.full_points > self.dealer.full_points:
+                    player.money += player.bet * 2
+                    if isinstance(player, Player.Bot):
+                        print(MESSAGES.get('win').format(player))
+                    elif isinstance(player, Player.Player):
+                        print('You are win')
+
+                elif player.full_points < self.dealer.full_points:
+                    if isinstance(player, Player.Bot):
+                        print(MESSAGES.get('lose').format(player))
+                    elif isinstance(player, Player.Player):
+                        print('You are lose')
+            # check with each player
+
+
+    def play_with_dealer(self):
+        while self.dealer.ask_card():
+            card = self.deck.get_card()
+            self.dealer.take_card(card)
+        self.dealer.print_cards()
+
+    # интерфейс на начало игры
+    def start_game(self):
+        message = MESSAGES.get('ask_start')  # берем сообщение из констант
+        if not self._ask_starting(message=message):  # если метод вернул фолс дропаем
+            exit(1)
+
+        # generating data for starting
+        self._launching()
+
+        while True:
+            # ask about bet
+            self.ask_bet()
+
+            # give first cards to the players
+            self.first_desc()
+
+            # print player cards after first deal
+            self.player.print_cards()
+
+            # ask_player_about_cards
+            self.ask_card()
+
+            self.play_with_dealer()
+
+            self.check_winner()
+
+            if not self._ask_starting(MESSAGES.get('rerun')):
+                break
+
+            # todo: change players pos
+            # todo: check all players for money
+            # todo:
+
+Game.asd = 10
---
+++ black_jack/Player.py
@@ -0,0 +1,107 @@
+import abc
+from Deck import Deck
+import random
+from const import MESSAGES, NAMES
+from picture import CardPrinter, CardFromDeck
+
+
+class AbstractPlayer(abc.ABC):
+
+    def __init__(self):
+        self.cards = []
+        # self.position = position
+        self.bet = 0
+        self.full_points = 0
+        self.money = 100
+
+    def change_points(self):
+        self.full_points = sum([card.points for card in self.cards])
+
+    def take_card(self, card):
+        self.cards.append(card)
+        self.change_points()
+
+    @abc.abstractmethod
+    def change_bet(self, max_bet, min_bet):
+        pass
+
+    @abc.abstractmethod
+    def ask_card(self):
+        pass
+
+    def print_cards(self):
+        print(self, " data")
+        self.hand_printer()  # hand printer
+        # for card in self.cards: # vs poocheridi
+        #     print(card)
+        print('Full points: ', self.full_points)
+
+    def hand_printer(self):
+        cards_list = list()
+        for card in self.cards:
+            card = CardFromDeck(card.suit, card.rank)  # unpack dictionary
+            cards_list.append(card)
+        else:
+            hand_print = CardPrinter.ascii_version_of_card(*cards_list)
+            print(hand_print)
+
+
+class Player(AbstractPlayer):
+
+    def change_bet(self, max_bet, min_bet):
+        while True:
+            value = int(input('Make your bet: '))
+            if value < max_bet and value > min_bet:
+                self.bet = value
+                self.money -= self.bet
+                break
+        print(f'Your bet is {self.bet}')
+
+    def ask_card(self):
+        if self.full_points == 21:
+            return False
+
+        choice = input(MESSAGES.get('ask_card'))
+        if choice == 'y':
+            return True
+        else:
+            return False
+
+
+class Bot(AbstractPlayer):
+
+    def __init__(self):
+        super().__init__()
+        self.max_points = random.randint(17, 20)
+
+    def change_bet(self, max_bet, min_bet):
+        self.bet = random.randint(min_bet, max_bet)
+        self.money -= self.bet
+        print(self, 'give: ', self.bet)
+
+    def ask_card(self):
+        if self.full_points < self.max_points:
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        name = random.choice(NAMES) + '_Bot'
+        return name
+
+
+class Dealer(AbstractPlayer):
+
+    max_points = 17
+
+    def change_bet(self, max_bet, min_bet):
+        """
+        NOTE: This type is Dealer so it has no bets
+        """
+        raise Exception('This type is dealer so its has no bets')
+
+    def ask_card(self):
+        if self.full_points < self.max_points:
+            return True
+        else:
+            return False
